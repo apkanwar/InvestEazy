b6a3d43d30f9d089b1d75e25df43a3366e3f2076
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2022-09-07 17:14:58 -0400

    Added description details for each property

--- components/propertyDetails/description.js
+++ components/propertyDetails/description.js
@@ -1,29 +1,33 @@
-import styles from '../../styles/property_details.module.css'
+import styles from '../../styles/description.module.css'
 import Image from 'next/image'
 import HotelImage from '../../public/hotel.webp'
 
 export default function PropertyDetails({ propertyData = [] }) {
   return (
-    <>
-      <div className={styles.banner}>
-      <div className={styles.bannerContent}>
-        <div className={styles.imageBox}>
-          <Image src={HotelImage} width={750} height={650} />
-        </div>
-        <div className={styles.info}>
-          <h2 className={styles.head}>{propertyData.name}</h2>
-          <h3 className={styles.address}>{propertyData.address}</h3>
-          <h3 className={styles.main}>{propertyData.detailedDescription}</h3>
+    < >
+      <div className={styles.section}>
+        <div className={styles.content_a}>
+          <div className={styles.imageBox}>
+            <Image src={HotelImage} width={750} height={650} />
+          </div>
+          <div className={styles.info}>
+            <h2 className={styles.head}>{propertyData.name}</h2>
+            <h3 className={styles.address}>{propertyData.address}</h3>
+            <p className={styles.main}>{propertyData.detailedDescription}</p>
 
-          <div className={styles.button}>
-            <div className={styles.button_text}>
-              Start Investing
-              <span className={styles.arrow}></span>
+            <div className={styles.button}>
+              <div className={styles.button_text}>
+                Start Investing
+                <span className={styles.arrow}></span>
+              </div>
             </div>
           </div>
         </div>
+        
+        <div className={styles.content_b}>
+          
+        </div>
       </div>
-    </div>
     </>
   )
 }
