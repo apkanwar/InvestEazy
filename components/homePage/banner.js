9340aa7b3b90214e35721b78485690b23784c085
Atinderpaul Kanwar <ap_kanwar@outlook.com>
2022-09-18 19:53:53 -0400

    Testing fonts

--- components/homePage/banner.js
+++ components/homePage/banner.js
@@ -5,16 +5,16 @@ export default function TopBanner() {
     <div className={styles.banner}>
       <div className={styles.bannerContent}>
         <div className={styles.info}>
-          <h2 className={styles.head}>Let’s Own Real Estate Together</h2>
-          <h3 className={styles.main}>Join thousands of Canadians already earning passive income with addy, get started with $1</h3>
+          <h2 className={styles.head}>Real Estate made Eazy</h2>
+          <h3 className={styles.main}>Join thousands of Canadians already earning passive income with InvestEazy, get started with $1</h3>
 
           <div className={styles.button}>
             <div className={styles.button_text}>Join Today</div>
           </div>
         </div>
-        <div className={styles.imageBox}>
+        {/* <div className={styles.imageBox}> */}
           {/* <Image src={CompanyLogo} width={48} height={48}/> */}
-        </div>
+        {/* </div> */}
       </div>
     </div>
   )
